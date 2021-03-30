3ff6ccceba22e4f16caf80043c44c7ac355b49bd
Дмитрий Пинчук <laik111@mail.ru>
2021-03-30 22:06:42 +0300

    add

--- Задание 1/Задание 1/Form1.Designer.cs
+++ Задание 1/Задание 1/Form1.Designer.cs
@@ -33,6 +33,8 @@ namespace Задание_1
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -56,7 +58,7 @@ namespace Задание_1
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(124, 36);
+            this.label1.Location = new System.Drawing.Point(110, 36);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(99, 13);
             this.label1.TabIndex = 2;
@@ -65,17 +67,39 @@ namespace Задание_1
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(393, 36);
+            this.label2.Location = new System.Drawing.Point(356, 36);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(142, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.label2.Text = "Преобразованный массив";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(124, 333);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Заполнить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(396, 333);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Рассчитать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
@@ -93,6 +117,8 @@ namespace Задание_1
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Задание 1/Задание 1/Form1.cs
+++ Задание 1/Задание 1/Form1.cs
@@ -12,9 +12,36 @@ namespace Задание_1
 {
     public partial class Form1 : Form
     {
+        private int[] mas = new int[15];
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            textBox1.Text = "";
+            for (int i = 0; i < 15; i++)
+            {
+                mas[i] = rand.Next(-50, 50);
+                textBox1.Text += "Mas[" + Convert.ToString(i) + "] = " + Convert.ToString(mas[i]) + Environment.NewLine;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int sum = 0;
+            textBox2.Text = "";
+            for (int i = 0; i < 15; i++)
+            {
+                if (mas[i] < 0) mas[i] = 0;
+                if (mas[i] % 2 == 0) sum += mas[i];
+                textBox2.Text += "Mas[" + Convert.ToString(i) + "] = " + Convert.ToString(mas[i]) + Environment.NewLine;
+            }
+
+            textBox2.Text += "Сумма чётных эл-ов = " + Convert.ToString(sum);
         }
     }
 }
