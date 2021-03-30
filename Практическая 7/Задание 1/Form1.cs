aee478e41cc538de31d995b9c50841e28c54d33f
Дмитрий Пинчук <laik111@mail.ru>
2021-03-31 00:31:00 +0300

    add ex 2(5)

---
+++ Практическая 7/Задание 1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задание_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.RowCount = 10; // кол-во строк
+            dataGridView1.ColumnCount = 10; // кол-во столбцов
+            
+            int[,] A = new int[10, 10];
+            int i, j;
+            Random rand = new Random();
+            for (i = 0; i < 10; i++)
+            for (j = 0; j < 10; j++)
+                A[i, j] = rand.Next(-100, 100);
+
+            for (i = 0; i < 10; i++)
+            for (j = 0; j < 10; j++)
+                dataGridView1.Rows[i].Cells[j].Value = Convert.ToString(A[i, j]);
+
+
+            int max = int.MinValue;
+            int min = int.MaxValue;
+
+            for (i = 0; i < 10; i++)
+                if (A[i, 9 - i] > max)
+                    max = A[i, 9 - i];
+
+            for (i = 0; i < 10; i++)
+                if (A[i, 9 - i] < min)
+                    min = A[i, 9 - i];
+
+            textBox1.Text += "Max >" + Convert.ToString(max) + Environment.NewLine;
+            textBox1.Text += "Min >" + Convert.ToString(min);
+        }
+    }
+}
---
+++ Практическая 7/Задание 2 (5)/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Задание_2__5_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            dataGridView1.RowCount = 10;
+            dataGridView1.ColumnCount = 10;
+
+            int[,] A = new int[10, 10];
+            int i, j;
+            Random rand = new Random();
+            for (i = 0; i < 10; i++)
+            for (j = 0; j < 10; j++)
+                A[i, j] = rand.Next(-100, 100);
+
+            for (i = 0; i < 10; i++)
+            for (j = 0; j < 10; j++)
+                dataGridView1.Rows[i].Cells[j].Value = Convert.ToString(A[i,j]);
+
+            /*Вычислить, в какой строке матрицы сумма элементов максимальная, а в
+            какой - минимальная.*/
+
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            int count = 0;
+            int RowMax = 0, RowMin = 0;
+            for (i = 0; i < 10; i++)
+            {
+                for (j = 0; j < 10; j++)
+                    count += A[i, j];
+                if (count > max)
+                {
+                    max = count;
+                    RowMax = i;
+                }
+
+                if (count < min)
+                {
+                    min = count;
+                    RowMin = i;
+                }
+
+                count = 0;
+            }
+
+            textBox1.Text += "Сумма максимальна в " + Convert.ToString(RowMax) + " строке"+ " и = " + Convert.ToString(max) + Environment.NewLine;
+            textBox1.Text += "Сумма минимальна в " + Convert.ToString(RowMin) + " строке" + " и = "+ Convert.ToString(min) + Environment.NewLine;
+        }
+    }
+}
